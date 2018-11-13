77fbbe83e574e3e19453a62a2e5b5de966eeecfa
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-13 12:05:42 +0100

    remove console statement

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 String.prototype.toSentenceCase = function() {
   const words = this.split(' ');
-  console.log('first element', words[0]);
   const transformedWords = words.map(word => {
     return word === words[0] ? `${word.charAt(0).toUpperCase()}${(word.slice(1)).toLowerCase()}` : word.toLowerCase();
   });
